fc3277555479ff60f3870e2173ec71d11b6ea90e
shiradahan150 <shiradahan150@gmail.com>
2025-10-29 13:17:21 +0200

    submit EX2

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,26 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  max_num = 0
+  if num1 >= num2:
+    max_num
+  else:
+    max_num = num2
+  if num3 >= max_num:
+    max_num = num3
+  
+  return max_num
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  sum = num1 + num2 + num3
+
+  return sum/3
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    data = [num1, num2, num3]
+    mean = find_mean(num1, num2, num3)
+
+    variance = sum((x - mean) ** 2 for x in data) / len(data)
+    std = variance ** 0.5
+
+    return mean, std
 
